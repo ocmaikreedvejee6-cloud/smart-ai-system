f31ea01365d296fd316f7e1a339fc64525b51893
Reedvejee H. Kiamco <ocmaikreedvejee6@gmail.com>
2026-04-04 16:51:58 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
 
+echo "Updating system..."
+sudo apt update
+
+echo "Installing OpenCV..."
+sudo apt install python3-opencv -y
+
+echo "Creating virtual environment..."
+python3 -m venv venv --system-site-packages
+
+source venv/bin/activate
+
+echo "Installing Python packages..."
+pip install --upgrade pip
+pip install ultralytics --no-deps --no-cache-dir
+pip install numpy pillow pyyaml requests
+
+echo "Running system..."
+python main.py
