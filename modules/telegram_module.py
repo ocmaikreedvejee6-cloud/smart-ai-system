ea14c21a0dc4a0e1d6ce3bf3048b99b840275d55
Reedvejee H. Kiamco <ocmaikreedvejee6@gmail.com>
2026-04-06 17:52:19 +0800

    Update telegram_module.py

--- modules/telegram_module.py
+++ modules/telegram_module.py
@@ -1,10 +1,14 @@
 import requests
 
-TOKEN = "YOUR_BOT_TOKEN"
-CHAT_ID = "YOUR_CHAT_ID"
+TOKEN = "8490765768:AAFU-Vpi0HAiS5_2V2mcboWYeiG8W4neiVE"
+CHAT_ID = "437470295"
 
 def send_alert(image_path):
     url = f"https://api.telegram.org/bot{TOKEN}/sendPhoto"
-
+    
     with open(image_path, "rb") as photo:
-        requests.post(url, data={"chat_id": CHAT_ID}, files={"photo": photo})
+        requests.post(
+            url,
+            data={"chat_id": CHAT_ID},
+            files={"photo": photo}
+        )
