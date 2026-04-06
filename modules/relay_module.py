120f8ffede9f94fa0a273c81b7406dcc72615dc8
Reedvejee H. Kiamco <ocmaikreedvejee6@gmail.com>
2026-04-06 17:04:55 +0800

    Update relay_module.py

--- modules/relay_module.py
+++ modules/relay_module.py
@@ -1,1 +1,19 @@
+import RPi.GPIO as GPIO
 
+RELAY_1 = 18
+RELAY_2 = 23
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(RELAY_1, GPIO.OUT)
+GPIO.setup(RELAY_2, GPIO.OUT)
+
+def turn_on():
+    GPIO.output(RELAY_1, GPIO.HIGH)
+    GPIO.output(RELAY_2, GPIO.HIGH)
+
+def turn_off():
+    GPIO.output(RELAY_1, GPIO.LOW)
+    GPIO.output(RELAY_2, GPIO.LOW)
+
+def cleanup():
+    GPIO.cleanup()
