4fe1ae1462a56da5a04811e962348795badfc149
Reedvejee H. Kiamco <ocmaikreedvejee6@gmail.com>
2026-04-06 17:05:22 +0800

    Update firebase_module.py

--- modules/firebase_module.py
+++ modules/firebase_module.py
@@ -1,1 +1,25 @@
+import firebase_admin
+from firebase_admin import credentials, db, storage
+from datetime import datetime
 
+cred = credentials.Certificate("serviceAccountKey.json")
+
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'YOUR_DB_URL',
+    'storageBucket': 'YOUR_BUCKET.appspot.com'
+})
+
+def upload_image(path, name):
+    bucket = storage.bucket()
+    blob = bucket.blob(f"intruders/{name}")
+    blob.upload_from_filename(path)
+    blob.make_public()
+    return blob.public_url
+
+def save_log(event, image_url):
+    ref = db.reference("logs")
+    ref.push({
+        "event": event,
+        "image": image_url,
+        "time": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    })
