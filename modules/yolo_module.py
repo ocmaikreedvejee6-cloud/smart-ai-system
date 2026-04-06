a69b5e45b5dc215ba3bee361ffae1992d55f1ee1
Reedvejee H. Kiamco <ocmaikreedvejee6@gmail.com>
2026-04-06 17:04:30 +0800

    Update yolo_module.py

--- modules/yolo_module.py
+++ modules/yolo_module.py
@@ -1,1 +1,14 @@
+from ultralytics import YOLO
 
+model = YOLO("best.pt")
+
+def detect_person(frame):
+    results = model(frame, conf=0.5, imgsz=416)
+
+    person_detected = False
+    for result in results:
+        for cls in result.boxes.cls:
+            if int(cls) == 0:
+                person_detected = True
+
+    return person_detected, results
